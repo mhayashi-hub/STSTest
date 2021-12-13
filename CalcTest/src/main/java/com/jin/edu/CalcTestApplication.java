a6efd6033be602b4ffe7d91397b7fed94d292587
mhayashi-hub <redbrick.gmad@gmail.com>
2021-12-13 12:50:34 +0900

    2021/12/13 STSでのテストプロジェクトのgitへの追加テスト

---
+++ CalcTest/src/main/java/com/jin/edu/CalcTestApplication.java
@@ -0,0 +1,13 @@
+package com.jin.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcTestApplication.class, args);
+	}
+
+}
---
+++ CalcTest/src/test/java/com/jin/edu/CalcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
