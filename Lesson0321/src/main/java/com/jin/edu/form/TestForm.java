8ff6472b92740381b77e3154a32ad0f3e30a024f
mhayashi-hub <redbrick.gmad@gmail.com>
2021-12-21 10:07:16 +0900

    2021/12/21 午前 Mapを使った派生実装のテスト、一応エラーとり切れて動作確認

--- Lesson0321/src/main/java/com/jin/edu/form/TestForm.java
+++ Lesson0321/src/main/java/com/jin/edu/form/TestForm.java
@@ -1,5 +1,7 @@
 package com.jin.edu.form;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestForm {
 
@@ -8,18 +10,28 @@ public class TestForm {
 	private Integer shainNo;
 //	member name Kanji
 	private String shainNameKanji;
-	
+	// setter method
 	public void setShainNo(Integer shainNo) {
 		this.shainNo = shainNo;
 	}
 	public void setShainNameKanji(String shainNameKanji) {
 		this.shainNameKanji = shainNameKanji;
 	}
-	
+	// getter method
 	public Integer getShainNo() {
 		return shainNo;
 	}
 	public String getShainNameKanji() {
 		return shainNameKanji;
+	}
+	// shainNoとshainNameKanjiを格納したhashMap
+	Map<Integer, String> shainListMap = new HashMap<> ();
+	// setter method
+	public void setShainListMap(Map<Integer, String> shainListMap) {
+		this.shainListMap = shainListMap;
+	}
+	// getter method
+	public Map<Integer, String> getShainListMap() {
+		return shainListMap;
 	}
 }
--- Lesson0321/src/main/java/com/jin/edu/service/TestService.java
+++ Lesson0321/src/main/java/com/jin/edu/service/TestService.java
@@ -9,25 +9,25 @@ import com.jin.edu.form.TestForm;
 @Service
 public class TestService {
 
-	Map<Integer, String> shainListMap = new HashMap<>();
-			shainListMap.put(001, "鈴木イチロー");
-			shainListMap.put(002, "田中将大");
-			shainListMap.put(003, "高橋雄介");
-			shainListMap.put(004, "鈴木亮一");
-			shainListMap.put(014, "橋本省吾");
-
 	/**
 	 * memberの情報設定
 	 * 
 	 * @param form 画面情報から取得
 	 */
 	public void setShainInfo(TestForm form) {
+
+		form.getShainListMap().put(001, "鈴木イチロー");
+		form.getShainListMap().put(002, "田中将大");
+		form.getShainListMap().put(003, "高橋雄介");
+		form.getShainListMap().put(004, "鈴木亮一");
+		form.getShainListMap().put(005, "橋本省吾");
 		
-		if (shainListMap.containsKey(form.getShainNo())) {
-			shainListMap.get(form.getShainNo());
+		if (form.getShainListMap().containsKey(form.getShainNo())) {
+			form.setShainNameKanji(form.getShainListMap().get(form.getShainNo()));
+			form.getShainNameKanji();
 		} else {
-			shainListMap.put(000, "該当する社員はいません");
-			shainListMap.get(000);
+			form.setShainNameKanji("該当する社員はいません");
+			form.getShainNameKanji();
 		}
 		// member 各情報を設定する。
 		// form.setShainNameKanji("鈴木イチロー");
